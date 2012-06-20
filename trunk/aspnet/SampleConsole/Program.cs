851552da1d261e148cf43266a1fc15537eb596d0
oi.u.sms@gmail.com <oi.u.sms@gmail.com@86fb3f93-7775-889a-f4d0-bc31b0a29b76>
2012-06-20 07:48:29 +0000

    Added ASP.NET windows console sample

---
+++ trunk/aspnet/SampleConsole/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Configuration;
+using System.Drawing;
+using System.Net.Mime;
+using System.Threading;
+using System.Windows.Forms;
+using GrabzIt;
+
+namespace SampleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please specify a URL to take picture of. For example http://www.google.com");
+                string url = Console.ReadLine();
+                GrabzItClient grabzIt = GrabzItClient.Create(ConfigurationManager.AppSettings["ApplicationKey"],
+                                                             ConfigurationManager.AppSettings["ApplicationSecret"]);
+                try
+                {
+                    string id = grabzIt.TakePicture(url);
+                    bool completed = false;
+                    while (!completed)
+                    {
+                        Image image = grabzIt.GetPicture(id);
+                        if (image != null)
+                        {
+                            string filename = url.Substring(url.IndexOf("://") + 3) + ".jpg";
+                            Console.WriteLine("Screenshot has been saved to: " + filename);
+                            image.Save(filename);
+                            completed = true;
+                        }
+                        Thread.Sleep(1000);
+                        Application.DoEvents();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.WriteLine("Do you wish to exit? (y/n)");
+                string exit = Console.ReadKey().KeyChar.ToString();
+                if (exit.ToLower() == "y")
+                {
+                    break;
+                }
+                Application.DoEvents();
+            }
+        }
+    }
+}
--- trunk/aspnet/readme.txt
+++ trunk/aspnet/readme.txt
@@ -1,12 +1,20 @@
 GrabzIt
+=======
 
 This software allows you to programmatically take screenshots of websites.
 
 Its usually best to place these files in thier own directory.
 
-To run the sample
+To run the samples
 
-Open web.config and change the application key and application secret to match your settings the first two parameters can be got from http://grabz.it/api
+Open the GrabzIt solution file.
+
+Screenshots in Web Applications
+===============================
+
+The solution contains a project called Sample, which is a example web application. To configure this do the following:
+
+Then open web.config and change the application key and application secret to match your settings the first two parameters can be got from http://grabz.it/api
 
   <appSettings>
     <add key="ApplicationKey" value="APPLICATION KEY"/>
@@ -22,5 +30,23 @@ Next add the GrabzIt http handler to the web.config with the location it is curr
 Ensure your application has read and write access to the "screenshots" directory.
 
 Finally run the Default.aspx to start taking screenshots.
+
+Screenshots in Windows Applications
+===================================
+
+The solution also contains a project called SampleConsole, which is a example windows console application. To configure this do the following:
+
+Then open App.config and change the application key and application secret to match your settings the first two parameters can be got from http://grabz.it/api
+
+  <appSettings>
+    <add key="ApplicationKey" value="APPLICATION KEY"/>
+    <add key="ApplicationSecret" value="APPLICATION SECRET"/>
+  </appSettings>
+  
+Finally run the console application to create screenshots in windows.
+
+
+
+
 
 More documentation can be found at: http://grabz.it/api/aspnet
