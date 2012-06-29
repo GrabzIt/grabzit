62a9cc4bb2e772150100a0961cfe6981664bf891
oi.u.sms@gmail.com <oi.u.sms@gmail.com@86fb3f93-7775-889a-f4d0-bc31b0a29b76>
2012-06-29 13:21:31 +0000

    Fix to make message only appear on postback.

--- php/index.php
+++ php/index.php
@@ -28,13 +28,16 @@ if (count($_POST) > 0)
 <p>Enter the URL of the website you want to take a screenshot of. Then resulting screenshot should be saved in the <a href="images/">images directory</a>. It may take a few seconds for it to appear!</p>
 <p>If nothing is happening check the <a href="http://grabz.it/account/diagnostics">diagnostics panel</a> to see if there is an error.</p>
 <?php
-if (!empty($message))
+if (count($_POST) > 0)
 {
-    ?><p><span style="color:red;font-weight:bold;"><?php echo $message; ?></span></p><?php
-}
-else
-{
-    ?><p><span style="color:green;font-weight:bold;">Processing screenshot.</span></p><?php
+	if (!empty($message))
+	{
+	    ?><p><span style="color:red;font-weight:bold;"><?php echo $message; ?></span></p><?php
+	}
+	else
+	{
+	    ?><p><span style="color:green;font-weight:bold;">Processing screenshot.</span></p><?php
+	}
 }
 ?>
 <label style="font-weight:bold;margin-right:1em;">URL </label><input text="input" name="url"/>
