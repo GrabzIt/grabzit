32ae83341278d17beb0d5313772e0caa3a1389e8
oi.u.sms@gmail.com <oi.u.sms@gmail.com@86fb3f93-7775-889a-f4d0-bc31b0a29b76>
2012-07-02 09:42:58 +0000

    Changed default settings file

--- trunk/php/GrabzItConfig.php
+++ trunk/php/GrabzItConfig.php
@@ -4,5 +4,5 @@ $grabzItApplicationKey = "APPLICATION KEY";
 //Get this value from http://grabz.it/api
 $grabzItApplicationSecret = "APPLICATION SECRET";
 //The absolute path that you have placed the GrabzItHandler.php on your website
-$grabzItHandlerUrl = "http://[your website]/GrabzItHandler.php";
+$grabzItHandlerUrl = "URL OF YOUR GrabzItHandler.php FILE (http://www.example.com/grabzit/GrabzItHandler.php)";
 ?>
