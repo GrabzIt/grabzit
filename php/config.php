bdf2bead5cfb31890df1544701e6a44a4e6896bb
oi.u.sms@gmail.com <oi.u.sms@gmail.com@86fb3f93-7775-889a-f4d0-bc31b0a29b76>
2013-03-18 21:08:39 +0000

    Renamed handler URL

--- php/config.php
+++ php/config.php
@@ -3,6 +3,6 @@
 $grabzItApplicationKey = "APPLICATION KEY";
 //Get this value from http://grabz.it/api
 $grabzItApplicationSecret = "APPLICATION SECRET";
-//The absolute path that you have placed the GrabzItHandler.php on your website
+//The absolute path that you have placed the handler.php on your website
 $grabzItHandlerUrl = "URL OF YOUR GrabzItHandler.php FILE (http://www.example.com/grabzit/handler.php)";
 ?>
