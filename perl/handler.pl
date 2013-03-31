3f40e2829c0d23b42f9ad003893c058ed8346197
oi.u.sms@gmail.com <oi.u.sms@gmail.com@86fb3f93-7775-889a-f4d0-bc31b0a29b76>
2013-03-31 10:34:44 +0000

    Minor fix

--- perl/handler.pl
+++ perl/handler.pl
@@ -6,7 +6,7 @@ use Config::IniFiles;
 use File::Spec;
 use GrabzIt::GrabzItClient;
 
-#This Perl file handles the GrabzIt callback
+# This Perl file handles the GrabzIt callback
 
 print "HTTP/1.0 200 OK";
 print "Content-type: text/html\r\n\r\n";
@@ -21,16 +21,17 @@ $message = $cgi->param("message");
 $customId = $cgi->param("customid");
 $id = $cgi->param("id");
 $filename = $cgi->param("filename");
+$format = $cgi->param("format");
 
-#Custom id can be used to store user ids or whatever is needed for the later processing of the
-#resulting screenshot
+# Custom id can be used to store user ids or whatever is needed for the later processing of the
+# resulting screenshot
 
 $grabzIt = new GrabzItClient($Config{applicationKey}, $Config{applicationSecret});
-$result = $grabzIt->GetPicture($id);
+$result = $grabzIt->GetResult($id);
 
 if ($result)
 {
-	#Ensure that the application has the correct rights for this directory.
+	# Ensure that the application has the correct rights for this directory.
 	open FILE, ">".File::Spec->catfile("results",$filename) or die $!; 
 	binmode FILE;
 	print FILE $result; 
