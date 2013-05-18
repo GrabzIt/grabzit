0445aac03477e7e02d7d87b558041cee8d672814
oi.u.sms@gmail.com <oi.u.sms@gmail.com@86fb3f93-7775-889a-f4d0-bc31b0a29b76>
2013-05-18 12:14:43 +0000

    [maven-release-plugin]  copy for tag java-2.0.0

---
+++ tags/java-2.0.0/src/it/grabz/grabzit/Response.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.grabz.grabzit;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+/**
+ *
+ * @author Skinners
+ */
+class Response {
+    @SuppressWarnings("unchecked")
+    public <T> T Parse(URLConnection connection, Class<T> clazz) throws IOException, JAXBException {
+        InputStream in = null;
+        try {
+            in = connection.getInputStream();
+            JAXBContext context = JAXBContext.newInstance(clazz);
+            Unmarshaller unmarshaller = context.createUnmarshaller();            
+            return (T) unmarshaller.unmarshal(in);
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+    }    
+}
