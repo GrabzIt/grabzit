f473c34f5146329ffb398c5bf47416b782c8bf1b
oi.u.sms@gmail.com <oi.u.sms@gmail.com@86fb3f93-7775-889a-f4d0-bc31b0a29b76>
2013-06-18 05:51:17 +0000

    Upgraded to use visual studio 2010

--- aspnet/GrabzIt/Enums/VerticalPosition.cs
+++ aspnet/GrabzIt/Enums/VerticalPosition.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace GrabzIt.Enums
+﻿namespace GrabzIt.Enums
 {
     public enum VerticalPosition
     {
