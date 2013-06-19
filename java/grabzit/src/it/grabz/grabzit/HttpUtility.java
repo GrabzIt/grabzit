5a226d7b2edc51f851c6fbabd66b9e6358944208
oi.u.sms@gmail.com <oi.u.sms@gmail.com@86fb3f93-7775-889a-f4d0-bc31b0a29b76>
2013-06-19 05:53:55 +0000

    Added more general error handling.

--- java/grabzit/src/it/grabz/grabzit/HttpUtility.java
+++ java/grabzit/src/it/grabz/grabzit/HttpUtility.java
@@ -22,11 +22,15 @@ class HttpUtility {
         URLConnection urlConnection = (URLConnection) url.openConnection();
         HttpURLConnection httpConnection = (HttpURLConnection) urlConnection;
         
-        if (httpConnection.getResponseCode() == 403)
+        int statusCode = httpConnection.getResponseCode();
+        if (statusCode == 403)
         {
             throw new Exception("Potential DDOS Attack Detected. Please wait for your service to resume shortly. Also please slow the rate of requests you are sending to GrabzIt to ensure this does not happen in the future.");
         }
-        
+        else if (statusCode >= 400)
+        {
+            throw new Exception("A network error occured when connecting to the GrabzIt servers.");
+        }
         return urlConnection;
     }
 
