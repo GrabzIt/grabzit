30bc72ae1c5a8c5d89868b90e63e37b658df4efc
oi.u.sms@gmail.com <oi.u.sms@gmail.com@86fb3f93-7775-889a-f4d0-bc31b0a29b76>
2014-06-29 19:43:14 +0000

    [maven-release-plugin]  copy for tag 2.1.3

---
+++ tags/2.1.3/src/it/grabz/grabzit/enums/Country.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package it.grabz.grabzit.enums;
+
+/**
+ *
+ * @author Administrator
+ */
+public enum Country {
+    UNITEDSTATES("US"),
+    UNITEDKINGDOM("UK"),
+    DEFAULT("");
+
+    private String value;
+
+    Country(String value)
+    {
+        this.value = value;
+    }
+
+    /**
+     * @return the value
+     */
+    public String getValue() {
+        return value;
+    }
+}
