7c1e51e35e45d9f7907fca3b97b07318209bcef2
GrabzIt <support@grabz.it>
2016-10-05 12:44:39 +0100

    Version 3
    Improved UI for version 3.

--- php/index.php
+++ php/index.php
@@ -6,39 +6,56 @@ $message = '';
 
 if (count($_POST) > 0)
 {
-        if (isset($_POST["delete"]) && $_POST["delete"] == 1)
-        {
-            $files = glob('results/*');
-            foreach($files as $file)
-            {
-                if(is_file($file))
-                    unlink($file);
-            }
-        }
-        else
-        {
+	if (isset($_POST["delete"]) && $_POST["delete"] == 1)
+	{
+		$files = glob('results/*');
+		foreach($files as $file)
+		{
+			if(is_file($file))
+				unlink($file);
+		}
+	}
+	else
+	{
 		$url = $_POST["url"];
+		$html = $_POST["html"];
 		$format = $_POST["format"];
+		$convert = $_POST["convert"];
+		
 		try
 		{
 			$grabzIt = new GrabzItClient($grabzItApplicationKey, $grabzItApplicationSecret);
 			if ($format == "pdf")
 			{
-			    $grabzIt->SetPDFOptions($url);
+				if ($convert == 'html')
+				{
+					$grabzIt->HTMLToPDF($html);
+				}
+				else
+				{
+					$grabzIt->URLToPDF($url);
+				}
 			}
 			else if ($format == "gif")
 			{
-				$grabzIt->SetAnimationOptions($url);
+				$grabzIt->URLToAnimation($url);
 			}
 			else
 			{
-			    $grabzIt->SetImageOptions($url);
+				if ($convert == 'html')
+				{
+					$grabzIt->HTMLToImage($html);
+				}
+				else
+				{
+					$grabzIt->URLToImage($url);
+				}
 			}
 			$grabzIt->Save($grabzItHandlerUrl);
 		}
 		catch (Exception $e)
 		{
-		    $message =  $e->getMessage();
+			$message =  $e->getMessage();
 		}
 	}
 }
@@ -54,7 +71,7 @@ if (count($_POST) > 0)
 <body>
 <h1>GrabzIt Demo</h1>
 <form method="post" action="index.php" class="inputForms">
-<p><span id="spnScreenshot">Enter the URL of the website you want to take a screenshot of. The resulting screenshot</span><span class="hidden" id="spnGif">Enter the URL of the online video you want to convert into a animated GIF. The resulting animated GIF</span> should then be saved in the <a href="results/" target="_blank">results directory</a>. It may take a few seconds for it to appear! If nothing is happening check the <a href="http://grabz.it/account/diagnostics" target="_blank">diagnostics panel</a> to see if there is an error.</p>
+<p><span id="spnScreenshot">Enter the HTML or URL you want to convert into a PDF or Image. The resulting capture</span><span class="hidden" id="spnGif">Enter the URL of the online video you want to convert into a animated GIF. The resulting animated GIF</span> should then be saved in the <a href="results/" target="_blank">results directory</a>. It may take a few seconds for it to appear! If nothing is happening check the <a href="http://grabz.it/account/diagnostics" target="_blank">diagnostics panel</a> to see if there is an error.</p>
 <?php
 if ($grabzItHandlerUrl == "URL OF YOUR handler.php FILE (http://www.example.com/grabzit/handler.php)")
 {
@@ -77,12 +94,26 @@ if (count($_POST) > 0 && !isset($_POST["delete"]))
 	}
 }
 ?>
-<label style="font-weight:bold;margin-right:1em;">URL </label><input text="input" name="url"/> <select name="format" onchange="selectChanged(this)">
+<div class="Row" id="divConvert">
+<label>Convert </label><select name="convert" onchange="selectConvertChanged(this)">
+  <option value="url">URL</option>
+  <option value="html">HTML</option>
+</select>
+</div>
+<div id="divHTML" class="Row hidden">
+<label>HTML </label><textarea name="html"><html><body><h1>Hello world!</h1></body></html></textarea>
+</div>
+<div id="divURL" class="Row">
+<label>URL </label><input text="input" name="url" placeholder="http://www.example.com"/>
+</div>
+<div class="Row">
+<label>Format </label><select name="format" onchange="selectChanged(this)">
   <option value="jpg">JPG</option>
   <option value="pdf">PDF</option>
   <option value="gif">GIF</option>
 </select>
-<input type="submit" value="Grabz It"></input>
+</div>
+<input type="submit" value="Grabz It" style="margin-left:12em"></input>
 </form>
 <form method="post" action="index.php" class="inputForms">
 <input type="hidden" name="delete" value="1"></input>
