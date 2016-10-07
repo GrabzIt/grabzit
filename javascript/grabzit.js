9647e6c75f8db1efd8678d5796538ceff21eeb4f
GrabzIt <support@grabz.it>
2016-10-07 11:12:06 +0100

    Version 3
    As JavaScript now supports converting HTML it will now use the
    JavaScript library rather than just specifiying a script tags. It has
    been designed to simplify the process of using GrabzIt's JavaScript API.

---
+++ javascript/grabzit.js
@@ -0,0 +1,153 @@
+function GrabzIt(key)
+{
+	return new (function(key)
+	{	
+		this.key = key;
+		this.data = null;
+		this.dataKey = null;
+		this.options = null;
+		this.post = false;
+		this.elem = null;
+		
+		this.ConvertURL = function(url, options)
+		{
+			this.data = url;
+			this.dataKey = 'url';
+			this.options = options;
+			this.post = false;
+			
+			return this;
+		};
+		
+		this.ConvertHTML = function(html, options)
+		{
+			this.data = html;
+			this.dataKey = 'html';
+			this.options = options;
+			this.post = true;
+
+			return this;			
+		};		
+		
+		this._post = function(qs)
+		{
+			var xhttp;
+			if (window.XMLHttpRequest) {
+				xhttp = new XMLHttpRequest();
+				} else {
+				xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			
+			var that = this;
+			
+			xhttp.onreadystatechange = function() {
+			  if (this.readyState == 4 && this.status == 200) {					  
+				  that.elem.appendChild(that._handlePost(JSON.parse(this.responseText)));
+			  }
+			};
+				
+			xhttp.open("POST", this._getBaseWebServiceUrl(), true);
+			xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			xhttp.send(qs);
+		};
+		
+		this._getBaseWebServiceUrl = function()
+		{
+			var protocol = '//';
+			if (window.location.protocol != 'https' && window.location.protocol != 'http')
+			{
+				protocol = 'http://';
+			}
+
+			return protocol + 'api.grabz.it/services/javascript.ashx';			
+		}
+		
+		this._createQueryString = function(sKey, sValue)
+		{			
+			var qs = 'key='+encodeURIComponent(this.key)+'&'+sKey+'=' + encodeURIComponent(sValue);
+		
+			if (this.options == null)
+			{
+				return qs;
+			}
+		
+			for(var k in this.options)
+			{
+				if (k != 'format' && k != 'cache' && k != 'customWaterMarkId' && k != 'quality'
+				&& k != 'country' && k != 'filename' && k != 'errorid' && k != 'errorclass' &&
+				k != 'onfinish' && k != 'onerror' && k != 'delay' && k != 'bwidth' && k != 'bheight' &&
+				k != 'height' && k != 'width' && k != 'target' && k != 'requestas' && k != 'download' && k != 'suppresserrors' && k != 'displayid' && k != 'displayclass' && k != 'background' && k != 'pagesize' && k != 'orientation' && k != 'includelinks' && k != 'includeoutline' && k != 'title' && k != 'coverurl' && k != 'mtop' && k != 'mleft' && k != 'mbottom' && k != 'mright' && k != 'tabletoinclude' && k != 'includeheadernames' && k != 'includealltables' && k != 'start' && k != 'duration' && k != 'speed' && k != 'fps' && k != 'repeat' && k != 'reverse')
+				{
+					throw "Option " + k + " not recognized!";
+				}
+				
+				var v = this.options[k];
+				if (v != null)
+                {
+					qs += '&' + k + '=' + encodeURIComponent(v);
+				}
+			}			
+			
+			return qs;
+		}
+		
+		this._createScriptNode = function(sUrl)
+		{
+			var scriptNode = document.createElement('script');
+			scriptNode.src = sUrl;
+
+			return scriptNode;
+		}
+		
+		this._handlePost = function(obj)
+		{
+			if (obj != null)
+			{
+				if (obj.ID == null || obj.ID == '')
+				{
+					throw obj.Message;
+				}
+				return this._createScriptNode(this._getBaseWebServiceUrl() + '?' + this._createQueryString('id', obj.ID));
+			}
+		}
+		
+		this.Create = function()
+		{
+			var defaultNode = document.documentElement;
+			if (document.body != null)
+			{
+				defaultNode = document.body;
+			}
+			this.AddTo(defaultNode);
+		};
+
+		this.AddTo = function(container)
+		{			
+			if (typeof container == 'string' || container instanceof String)
+			{
+				this.elem = document.getElementById(container);
+				if (this.elem == null)
+				{
+					throw "An element with the id " + container + " was not found";
+				}
+			}
+			else if (container.nodeType === 1)
+			{
+				this.elem = container;
+			}
+
+			if (this.elem == null)
+			{
+				throw "No valid element was provided to attach the screenshot to";
+			}			
+			
+			if (this.post)
+			{
+				this._post(this._createQueryString(this.dataKey, this.data));
+				return;
+			}
+			
+			this.elem.appendChild(this._createScriptNode(this._getBaseWebServiceUrl() + '?' + this._createQueryString(this.dataKey, this.data)));
+		}
+	})(key);
+}
---
+++ javascript/readme.txt
@@ -0,0 +1,45 @@
+GrabzIt 3.00
+============
+
+This library allows you to dynamically add a screenshot to any HTML element or webpage.
+
+An example of the GrabzIt in action can be found in the demo.html file. Remember to replace the "Your Application Key" with your actual
+application key found here: http://grabz.it/api/
+
+GrabzIt JavaScript library provides three methods:
+
+- ConvertURL([url to capture], [options])
+- ConvertHTML([html to convert], [options])
+- AddTo([element | element id])
+- Create()
+
+The ConvertURL or ConvertHTML must be called and then the AddTo or Create method must be called. The AddTo method must specify the id of a element or the element 
+were the capture should be displayed, so the first two calls below will work. The Create method just creates the capture on the body tag or the root element of 
+the HTML document if the body tag doesn't exist. An example of this is shown in calls three and four below. 
+
+
+	<html>
+	<body>
+	<div id="screenshot"></div>
+	<script>
+	//Call One
+	GrabzIt("YOUR APPLICATION KEY").ConvertURL('http://www.google.com').AddTo('screenshot');
+
+	//Call Two
+	var elem = document.getElementById('screenshot');
+	GrabzIt("YOUR APPLICATION KEY").ConvertHTML('<h1>Hello World</h1>').AddTo(elem);
+	
+	//Call Three
+	GrabzIt("YOUR APPLICATION KEY").ConvertURL('http://www.google.com').Create();
+	
+	//Call Four
+	GrabzIt("YOUR APPLICATION KEY").ConvertHTML('<h1>Hello World</h1>').Create();	
+	</script>
+	</body>
+	</html>
+
+
+The options parameter takes all of the parameters found here (excluding the URL and application key parameters): http://grabz.it/api/javascript/parameters.aspx in the options object. For instance if you wanted to set the width and height to be 256 x 200 you could do this like so:
+
+
+	GrabzIt("YOUR APPLICATION KEY").ConvertURL('http://www.google.com', {"width": 250, "height": 200}).AddTo('div_id');
