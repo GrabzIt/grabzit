e759ae78e219e6b495f7cb14447c03f2948a777a
GrabzIt <support@grabz.it>
2017-05-26 15:26:58 +0100

    Minor changes

--- python/setup.py
+++ python/setup.py
@@ -4,10 +4,10 @@ from distutils.core import setup
 
 setup(name='GrabzIt',
       version='3.1.0',
-      summary='Convert URLs or HTML to images, DOCX documents and PDFs',
       author='GrabzIt',
+      author_email='support@grabz.it',
       url='https://grabz.it/api/python/',
       license='MIT Licence',
-      description='This library allows you to programmatically convert web pages and HTML into images, DOCX documents, PDFs, CSVs and spreadsheets. Additionally GrabzIt allows you to convert online videos into animated GIFs',
+      description='GrabzIt enables allows you to programmatically convert web pages and HTML into images, DOCX documents, PDFs, CSVs and spreadsheets. Additionally GrabzIt allows you to convert online videos into animated GIFs',
       packages=['GrabzIt']      
 )
