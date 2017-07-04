81c4a33cd491533ed38bddc93025876f4c0b1b7d
GrabzIt <support@grabz.it>
2017-07-04 20:14:55 +0100

    Added transparency to image captures for the web recorder plugin.

--- plugins/javascript/state-store/grabzitwebrecorder.js
+++ plugins/javascript/state-store/grabzitwebrecorder.js
@@ -118,7 +118,7 @@ function GrabzIt(key)
 				&& k != 'country' && k != 'filename' && k != 'errorid' && k != 'errorclass' &&
 				k != 'onfinish' && k != 'onerror' && k != 'delay' && k != 'bwidth' && k != 'bheight' &&
 				k != 'height' && k != 'width' && k != 'target' && k != 'requestas' && k != 'download' && k != 'suppresserrors' && k != 'displayid' && k != 'displayclass' && k != 'background' && k != 'pagesize' && k != 'orientation' && k != 'includelinks' && k != 'includeoutline' && k != 'title' && k != 'coverurl' && k != 'mtop' && k != 'mleft' && k != 'mbottom' && k != 'mright' && k != 'tabletoinclude' && k != 'includeheadernames' && k != 'includealltables' && k != 'start' && k != 'duration' && k != 'speed' && k != 'fps' && k != 'repeat' && k != 'reverse' &&
-				k != 'templateid' && k != 'noresult' && k != 'hide' && k != 'includeimages' && k != 'export' && k != 'waitfor')
+				k != 'templateid' && k != 'noresult' && k != 'hide' && k != 'includeimages' && k != 'export' && k != 'waitfor' && k != 'transparent')
 				{
 					throw "Option " + k + " not recognized!";
 				}
