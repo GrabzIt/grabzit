d3128f1b4c8d59cef07c568019cf78917b2e8d6c
GrabzIt <support@grabz.it>
2018-01-18 21:37:11 +0000

    Update for missed code

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='GrabzIt',
-      version='3.2.8',
+      version='3.2.8.1',
       author='GrabzIt',
       author_email='support@grabz.it',
       url='https://grabz.it/api/python/',
