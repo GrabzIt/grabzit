eb5605a029a075f3b0b5dfc39d78dd2005a72579
GrabzIt <support@grabz.it>
2018-05-04 10:58:04 +0100

    Updated Python version number.

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='GrabzIt',
-      version='3.2.9',
+      version='3.2.9.1',
       author='GrabzIt',
       author_email='support@grabz.it',
       url='https://grabz.it/api/python/',
