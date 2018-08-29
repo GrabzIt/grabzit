43ac8a6470751675a7462d9300d78c3c2d65fd4e
GrabzIt <support@grabz.it>
2018-08-29 08:13:05 +0100

    n

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?php
 //Get this value from https://grabz.it/api
-$grabzItApplicationKey = "c3VwcG9ydEBncmFiei5pdA==";
+$grabzItApplicationKey = "APPLICATION KEY";
 //Get this value from https://grabz.it/api
-$grabzItApplicationSecret = "AD8/aT8/Pz8/Tz8/PwJ3Pz9sVSs/Pz8/Pz9DOzJodoi=";
+$grabzItApplicationSecret = "APPLICATION SECRET";
 //The absolute path that you have placed the handler.php on your website
 $grabzItHandlerUrl = "URL OF YOUR GrabzItHandler.php FILE (http://www.example.com/grabzit/handler.php)";
 ?>
