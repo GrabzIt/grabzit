33e0a599cc941b5a9420775debc1c6e6019723dd
GrabzIt <support@grabz.it>
2018-09-05 22:21:33 +0100

    Add HTML table to Python example.

--- python/ajax/ui.js
+++ python/ajax/ui.js
@@ -1,67 +1,77 @@
-var timeout = null;
+function UI(ajaxUrl, resourceDir){
+	this.timeout = null;
 
-function checkForResults() {
-	//clear div
-	$('#divResults').html('');
-	//read result directory and display
-	$.getJSON('ajax/results.py?r='+Math.floor((Math.random()*100000)+1), function(data) {
-		$.each(data, function(key, val) {
-			if (val.indexOf(".pdf") !== -1)
-			{
-				$('#divResults').append('<a title="Click to open" target="_blank" href="'+val+'"><img class="result" src="css/pdf.png"></img></a>');
-			}
-			else if (val.indexOf(".docx") !== -1)
-			{
-				$('#divResults').append('<a title="Click to open" target="_blank" href="'+val+'"><img class="result" src="css/docx.png"></img></a>');
-			}	
-			else
-			{
-				$('#divResults').append('<img title="Click to zoom in" class="result" onclick="zoom(\''+val+'\')" src="'+val+'"></img>');
-			}
+	this.checkForResults = function() {
+		//clear div
+		$('#divResults').html('');
+		//read result directory and display
+		$.getJSON(ajaxUrl+Math.floor((Math.random()*100000)+1), function(data) {
+			$.each(data, function(key, val) {
+				if (val.indexOf(".pdf") !== -1)
+				{
+					$('#divResults').append('<a title="Click to open" target="_blank" href="'+val+'"><img class="result" src="'+resourceDir+'/pdf.png"></img></a>');
+				}
+				else if (val.indexOf(".docx") !== -1)
+				{
+					$('#divResults').append('<a title="Click to open" target="_blank" href="'+val+'"><img class="result" src="'+resourceDir+'/docx.png"></img></a>');
+				}	
+				else if (val.indexOf(".csv") !== -1)
+				{
+					$('#divResults').append('<a title="Click to open" target="_blank" href="'+val+'"><img class="result" src="'+resourceDir+'/csv.png"></img></a>');
+				}				
+				else
+				{
+					$('#divResults').append('<img title="Click to zoom in" class="result" onclick="ui.zoom(\''+val+'\')" src="'+val+'"></img>');
+				}
+			});
 		});
+		clearTimeout(this.timeout);
+		this.timeout = setTimeout("ui.checkForResults()", 1000);
+	}
+
+	this.zoom = function(url)
+	{
+		clearTimeout(timeout);
+		$('#divResults').html('<img class="zoomedResult" title="Click to zoom out" onclick="ui.zoomout()" src="'+url+'">');
+	}
+
+	this.zoomout = function()
+	{
+		checkForResults();
+	}
+
+	this.selectChanged = function(select) {
+		if ($(select).val() == 'gif') {
+			$('#spnIntro').html('Enter the URL of the online video you want to convert into a animated GIF. The resulting animated GIF');
+			$('#divURL').show();
+			$('#divHTML').hide();
+			$('#divConvert').hide();	
+		}
+		else if ($(select).val() == 'csv') {
+			$('#spnIntro').html('Enter HTML or a URL that contains a HTML table, that you want to convert into a CSV. The resulting document');
+			$('#divConvert select').change();
+			$('#divConvert').show();	
+		}
+		else {
+			$('#spnIntro').html('Enter the HTML or URL you want to convert into a DOCX, PDF or Image. The resulting capture');
+			$('#divConvert select').change();
+			$('#divConvert').show();
+		}
+	}
+
+	this.selectConvertChanged = function(select) {
+		if ($(select).val() == 'url') {
+			$('#divURL').show();
+			$('#divHTML').hide();
+		}
+		else {
+			$('#divHTML').show();
+			$('#divURL').hide();
+		}
+	}
+
+	var that = this;
+	$(document).ready(function() {
+		that.checkForResults();
 	});
-	clearTimeout(timeout);
-	timeout = setTimeout("checkForResults()", 1000);
 }
-
-function zoom(url)
-{
-	clearTimeout(timeout);
-	$('#divResults').html('<img class="zoomedResult" title="Click to zoom out" onclick="zoomout()" src="'+url+'">');
-}
-
-function zoomout()
-{
-	checkForResults();
-}
-
-function selectChanged(select) {
-    if ($(select).val() == 'gif') {
-        $('#spnGif').show();
-		$('#divURL').show();
-		$('#divHTML').hide();
-		$('#divConvert').hide();		
-        $('#spnScreenshot').hide();
-    }
-    else {
-		$('#divConvert select').change();
-		$('#divConvert').show();
-        $('#spnScreenshot').show();
-        $('#spnGif').hide();
-    }
-}
-
-function selectConvertChanged(select) {
-    if ($(select).val() == 'url') {
-		$('#divURL').show();
-		$('#divHTML').hide();
-    }
-    else {
-		$('#divHTML').show();
-		$('#divURL').hide();
-    }
-}
-
-$(document).ready(function() {
-	checkForResults();
-});
