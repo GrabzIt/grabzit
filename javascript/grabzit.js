9e6f2b2ebf99a3449b6dcaed28e52dab8073cb78
GrabzIt <support@grabz.it>
2019-04-02 12:54:07 +0100

    Added onstart event to JavaScript API

--- javascript/grabzit.js
+++ javascript/grabzit.js
@@ -467,6 +467,12 @@ function GrabzIt(key)
 				window[functionName] = results['onerror'];
 				results['onerror'] = functionName;
 			}
+			
+			if (typeof(results['onstart']) === 'function'){
+				var functionName = 'grabzItOnStart' + Math.floor(Math.random() * (1000000000+1));
+				window[functionName] = results['onstart'];
+				results['onstart'] = functionName;
+			}	
 
 			return results;
 		}
@@ -557,7 +563,7 @@ function GrabzIt(key)
 				k != 'onfinish' && k != 'onerror' && k != 'delay' && k != 'bwidth' && k != 'bheight' &&
 				k != 'height' && k != 'width' && k != 'target' && k != 'requestas' && k != 'download' && k != 'suppresserrors' && k != 'displayid' && k != 'displayclass' && k != 'background' && k != 'pagesize' && k != 'orientation' && k != 'includelinks' && k != 'includeoutline' && k != 'title' && k != 'coverurl' && k != 'mtop' && k != 'mleft' && k != 'mbottom' && k != 'mright' && k != 'tabletoinclude' && k != 'includeheadernames' && k != 'includealltables' && k != 'start' && k != 'duration' && k != 'speed' && k != 'fps' && k != 'repeat' && k != 'reverse' &&
 				k != 'templateid' && k != 'noresult' && k != 'hide' && k != 'includeimages' && k != 'export' && k != 'waitfor' && k != 'transparent' &&
-				k != 'encryption' && k != 'post' && k != 'noads' && k != 'tvars' && k != 'proxy' && k != 'mergeid' && k != 'address' && k != 'nonotify' && k != 'cachelength')
+				k != 'encryption' && k != 'post' && k != 'noads' && k != 'tvars' && k != 'proxy' && k != 'mergeid' && k != 'address' && k != 'nonotify' && k != 'cachelength' && k != 'onstart')
 				{
 					var error = "Option " + k + " not recognized!";
 					document.documentElement.appendChild(this._createErrorMessage(error, null));
