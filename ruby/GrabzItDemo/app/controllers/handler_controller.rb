89ff92841eee413af395e939cb01ed6b3a22d18a
GrabzIt <support@grabz.it>
2019-05-07 20:52:40 +0100

    Added targetError to ruby handler.

--- ruby/GrabzItDemo/app/controllers/handler_controller.rb
+++ ruby/GrabzItDemo/app/controllers/handler_controller.rb
@@ -7,6 +7,7 @@ class HandlerController < ApplicationController
 	  id = params[:id]
 	  filename = params[:filename]
 	  format = params[:format]
+	  targetError = params[:targeterror]
 	  
   	  app_config = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
   	  grabzItClient = GrabzIt::Client.new(app_config['application_key'], app_config['application_secret'])
