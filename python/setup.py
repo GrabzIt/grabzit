9d045e2108792ed5f362270aaa2dd05b6ca057a4
GrabzIt <support@grabz.it>
2021-10-14 15:24:36 +0100

    Updated version

--- python/setup.py
+++ python/setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.txt')) as f:
     long_description = f.read()
 
 setup(name='GrabzIt',
-      version='3.5.1',
+      version='3.5.2',
       author='GrabzIt',
       author_email='support@grabz.it',
       url='https://grabz.it/api/python/',
